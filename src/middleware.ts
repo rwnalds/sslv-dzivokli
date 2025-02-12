3d1c9e7c05792d3107b7bdb2d1368ab484bc92e1
Ronalds Palacis <66126144+notdevkey@users.noreply.github.com>
2025-02-12 17:14:08 +0200

    Experimental edge runtime (#8)
    * feat: copywriting
    
    * feat: base url
    
    * feat: allow only users who have paid
    
    * feat: middleware paywall
    
    * chore: remove layout middleware
    
    * chore: runtime experimental edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,3 +25,5 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
+
+export const runtime = "experimental-edge";

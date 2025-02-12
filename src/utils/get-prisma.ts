bbb965a198af4f6d98c640259ef3a23f7a01dd3e
Ronalds Palacis <ronalds.palacis@gmail.com>
2025-02-12 17:31:58 +0200

    chore: prisma client test

--- src/utils/get-prisma.ts
+++ src/utils/get-prisma.ts
@@ -1,20 +1,7 @@
-import { neonConfig, Pool } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import ws from "ws";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-neonConfig.webSocketConstructor = ws;
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-neonConfig.poolQueryViaFetch = true;
-// Type definitions
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-if (process.env.NODE_ENV === "development") globalForPrisma.prisma = prisma;
-export { prisma };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

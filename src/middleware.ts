20950c2aaccbd5d6ce2fd501c2b5022ef168bd15
Ronalds Palacis <ronalds.palacis@gmail.com>
2025-02-17 18:23:34 +0200

    test: experimental edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,3 +26,5 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/dashboard/:path*", "/api/:path*"],
 };
+
+export const runtime = "experimental-edge";
